7c399e0b6706601f643d7bc11503d9e916ff754e
Kambale Kibeho <dessaikibeho@gmail.com>
2022-04-06 11:56:28 +0300

    my changes

---
+++ my-app/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  
+return (
+    
+    <div className="App">
+      <header className="App-header">
+        <img src='https://kdessaik.github.io/MY-web-site-BIO/pages/dessai3.jpg' className="App-logo" alt="logo"  />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Dessai Kibeho Biographie
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App
